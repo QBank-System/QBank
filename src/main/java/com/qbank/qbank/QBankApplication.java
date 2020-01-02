e2d3c5e469f967d7900026c0ce3842f873e775df
J <858335157@qq.com>
2020-01-02 12:01:54 +0800

    第一次提交

---
+++ src/main/java/com/qbank/qbank/QBankApplication.java
@@ -0,0 +1,13 @@
+package com.qbank.qbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QBankApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qbank/qbank/test/Test.java
@@ -0,0 +1,16 @@
+package com.qbank.qbank.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wangyujie
+ */
+@Controller
+public class Test {
+
+    @RequestMapping("/Index")
+    public String index(){
+        return "index";
+    }
+}
