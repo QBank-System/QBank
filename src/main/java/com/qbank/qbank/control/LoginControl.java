9b0e40cf66efb62d9f4489858f5c8a2f23af208e
J <858335157@qq.com>
2020-01-12 22:58:31 +0800

    改bug
    前一次提交在本地没问题，但是上传服务器出现了一堆bug，已经修改

--- src/main/java/com/qbank/qbank/control/LoginControl.java
+++ src/main/java/com/qbank/qbank/control/LoginControl.java
@@ -3,10 +3,15 @@ package com.qbank.qbank.control;
 import com.alibaba.fastjson.JSONObject;
 import com.qbank.qbank.dto.MvcDataDto;
 import com.qbank.qbank.service.impl.UserServiceImpl;
+import com.qbank.qbank.utils.DatabaseOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
 /**
@@ -18,6 +23,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginControl {
 
     private UserServiceImpl userService = new UserServiceImpl();
+    private static String homeUrl = "http://localhost";
+
+    static {
+        Properties properties = new Properties();
+        InputStream is = DatabaseOperations.class.getClassLoader().getResourceAsStream("application.properties");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        homeUrl = properties.getProperty("homeUrl");
+    }
+
 
     @RequestMapping("/Index")
     public String index() {
@@ -28,8 +46,8 @@ public class LoginControl {
     @ResponseBody
     public JSONObject login(@RequestParam("work_number") String work_number, @RequestParam("password") String password) throws Exception {
         MvcDataDto data = userService.login(work_number, password);
-        if(MvcDataDto.SUCCESS.equals(data.getResultCode())){
-            data.setRedirectUrl("/Main");
+        if (MvcDataDto.SUCCESS.equals(data.getResultCode())) {
+            data.setRedirectUrl(homeUrl + "/Main");
         }
         return data.toJson();
     }
---
+++ src/main/resources/static/js/webProperties.js
@@ -0,0 +1,4 @@
+function getHostUrl(){
+    return "http://localhost"
+    //return "http://47.101.209.223/QBank"
+}
