54b820090e259c2eb9eae8cc2465bc1a8dee12fc
J <858335157@qq.com>
2020-01-14 22:44:54 +0800

    使用thymeleaf方便前端开发和配置热部署

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    const user = sessionStorage.getItem("QbankUser");
+    if (user !== null) {
+        window.location.href = "/QBank/main";
+    }
+};
+
+function init() {
+    const work_number = document.getElementById("user_work_number").value;
+    const password = document.getElementById("user_password").value;
+    if (work_number === null) {
+        alert("请输入完整的职工号")
+    } else if (password === null) {
+        alert("请输入完整的密码")
+    } else {
+        const url = "/QBank/login/login";
+        const data = {"user_work_number": work_number, "user_password": sha1(password)};
+        $.ajax({
+            url: url,
+            dataType: "json",
+            async: true,
+            data: data,
+            type: "GET",
+            success: function (data) {
+                alert(data["resultMessage"]);
+                if ("success" === data["resultCode"]) {
+                    //TODO 加密
+                    sessionStorage.setItem("QbankUser", JSON.stringify(data["resultObj"]));
+                    window.location.href = data["redirectUrl"];
+                }
+            }
+        });
+    }
+}
